637388cc2274a273db8d30fed7d06234c3d127a7
vinayfishyhub <vinay.fishyhub@gmail.com>
2025-04-13 14:20:33 +0530

    deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,9 +23,7 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async() => {
-  await connectDB();
-  console.log(`Server running on port ${PORT}`);
-});
+connectDB()
+
+export default app;
